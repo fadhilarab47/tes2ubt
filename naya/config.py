9979d55afc1918b12c7efc09f2153915bf88b228
Opet321 <90963402+Opet321@users.noreply.github.com>
2023-09-30 15:17:01 +0700

    Add files via upload

---
+++ naya/config.py
@@ -0,0 +1,29 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+
+API_ID = int(getenv("API_ID", "209235"))
+OWNER = int(getenv("OWNER", "6678456418"))
+API_HASH = getenv("API_HASH", "169ee702e1df4b6e66d80311db36cc43")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://udoh:udoh@cluster0.sdmodpx.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "6312039005:AAGI-qW2qlHc3dXR56Q-Euu1d3Kqa8drHNo")
+OPENAI_API = getenv("OPENAI_API", "")
+GIT_TOKEN = getenv("GIT_TOKEN", "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")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", "")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", "")
+BRANCH = getenv("BRANCH", "naya")  # don't change
+REPO_URL = getenv("REPO_URL", "https://github.com/opet321/opet")
+CMD_HNDLR = getenv("CMD_HNDLR", ",")
+SESSION1 = getenv("SESSION1", "BQADMVMAVFXcO6ZeD9cJ0h-4HikszZwgJ6p6COYECFxcCNws8FmFTP97iIHCyK3uveqcCjNKGUBi22fu1-5GFxizF7fvYhBkHbVbtV1y9xrxUQOM5e1C0aXI762n-T8_pWraJLoaabZ4dIAv9kd88fNWNYnqv6bdk7glUnx3quJEbS0_GNoegUYcuRWbnXeV6yMJKzP7T0pHAekXxnYz28z9KLeJynaptiYl8d4XFLV4j_NFkDu2h9el_g4pjNgDFwVxOMjUfIIJkVr1_eGRdzYZ1z3qN40_g_ea6fZd-RxkJHQPM9QL-wuxQo8Hr8-Kyd-MHHjkctYB4odVncNEDMMwYgKn2wAAAAGOEShiAA")
+SESSION2 = getenv("SESSION2", "BQADMVMAM4rq0vi9Ae62oF7T5JsDFc0G8iseHWJQU1xqHM1MMBFgs_adyj9jrzud9H1MSm7SNW72AHt1yhmBrcxzxe56rkOMtwIm9YQdyS3ti4Hb7f_H_VqS_kSG7CFJtSj5ri6AjAwdvIlm0seTAw2_KlTmc7apsmsgg3rQUnUK3oxZoQ5Y9GY79gBviRTVXI_TuMYlJl9JGRXPHcFt-nDGUIzlAujgr3bgvdbG1_BtcA1Qb200OcHjWsdOFD07QlGlgJpOVF_tVvZlfxD12jSNQqAcrZIrHsCHDNhIo0LaQiqg64sAPLwCsgwtJIjkvbQXL896xpY56DD7Z7EYb7optgzy1gAAAAFw3mctAA")
+SESSION3 = getenv("SESSION3", "")
+SESSION4 = getenv("SESSION4", "")
+SESSION5 = getenv("SESSION5", "")
+SESSION6 = getenv("SESSION6", "")
+SESSION7 = getenv("SESSION7", "")
+SESSION8 = getenv("SESSION8", "")
+SESSION9 = getenv("SESSION9", "")
+SESSION10 = getenv("SESSION10", "")
