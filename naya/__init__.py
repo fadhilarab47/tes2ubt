17dc2ae84c0880a9d5b05cf7b282e73b0aba4e29
Opet321 <90963402+Opet321@users.noreply.github.com>
2023-10-08 00:25:03 +0700

    Update __init__.py

--- naya/__init__.py
+++ naya/__init__.py
@@ -19,7 +19,7 @@ from telegraph import Telegraph, exceptions, upload_file
 
 from .log import LOGGER
 
-from .config import (API_HASH, API_ID, BOT_TOKEN, CMD_HNDLR, SESSION1,
+from .config import (API_HASH, API_ID, BOT_TOKEN, SESSION1,
                      SESSION2, SESSION3, SESSION4, SESSION5, SESSION6,
                      SESSION7, SESSION8, SESSION9, SESSION10)
 
